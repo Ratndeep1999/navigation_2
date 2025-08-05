dc1fbd175b9203d02e964163097af76cf37eef5c
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-05 14:42:56 +0530

    string_routes.dart : static routes  add

--- lib/Routes/string_routes.dart
+++ lib/Routes/string_routes.dart
@@ -1,4 +1,6 @@
 class RouteName{
   static const String firstScreen = '/';
   static const String secondScreen = '/second_page';
+  static const String thirdScreen = '/third_page';
+  static const String fourthScreen = '/fourth_page';
 }
