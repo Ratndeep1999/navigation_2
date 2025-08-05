89c8e8936426bd940811688e3fc18fb20061a058
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-05 14:41:16 +0530

    first_page.dart : it navigate to second screen and pass data

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_2/Routes/string_routes.dart';
 
 class FirstPage extends StatefulWidget {
   const FirstPage({super.key});
@@ -11,8 +12,40 @@ class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(child: Column(children: [],)),
+      appBar: AppBar(title: Text('First screen'), centerTitle: true),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('navigate to second screen', style: TextStyle(fontSize: 15)),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 40,
+                width: 200,
+                child: OutlinedButton(
+                  onPressed: () {
+                    // it suggest secondScreen directly
+                    Navigator.pushNamed(
+                      context,
+                      RouteName.secondScreen,
+                      arguments: {
+                        // 'title' : 'Navigation',
+                        'name': 'Ratndeep',
+                        'age': 26
+                      },
+                    );
+                  },
+                  child: Text(
+                    'Second screen>>',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
