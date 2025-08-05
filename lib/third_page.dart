67a1669a69cf159adebe2ef5ee5d63b8491e08bf
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-08-05 14:42:30 +0530

    third_page.dart : previous page data received and popUntil button

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_2/Routes/string_routes.dart';
 
 class ThirdPage extends StatefulWidget {
-  const ThirdPage({super.key});
+  final Map? data;  // data receiving
+
+  const ThirdPage({super.key, required this.data});
 
   @override
   State<ThirdPage> createState() => _ThirdPageState();
@@ -10,6 +13,42 @@ class ThirdPage extends StatefulWidget {
 class _ThirdPageState extends State<ThirdPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third screen'),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Text('Name : ${widget.data?['name']}', style: TextStyle(fontSize: 30),),
+              Text('Age : ${widget.data?['age']}', style: TextStyle(fontSize: 30),),
+              Text('Gender : ${widget.data?['gender']}', style: TextStyle(fontSize: 30),),
+              SizedBox(height: 40,),
+
+              Text('Pop to first page', style: TextStyle(fontSize: 15)),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 40,
+                width: 200,
+                child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.popUntil(
+                      context,
+                      ModalRoute.withName(RouteName.firstScreen),
+                    );
+                  },
+                  child: Text('<<First Page', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
